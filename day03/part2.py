277913726603d44881e7853faf4c28ae1fa7cfbd
Fabian Lamers <fabian.lamers@capgemini.com>
2022-12-03 18:21:13 +0100

    day03

---
+++ day03/part2.py
@@ -0,0 +1,64 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def compute(s: str) -> int:
+    rtn_val = 0
+    lines = s.splitlines()
+    letter = 0
+    for line in lines:
+        line_length = len(line)
+        comp1 = line[:line_length//2]
+        comp2 = line[line_length//2:]
+        for i in comp1:
+            if i in comp2:
+                letter = i
+                break
+        if letter.islower():
+            rtn_val += ord(letter) - 96
+        else:
+            rtn_val += ord(letter) - 38
+
+    return rtn_val
+
+
+INPUT_S = """\
+vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw
+"""
+EXPECTED = 157
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
